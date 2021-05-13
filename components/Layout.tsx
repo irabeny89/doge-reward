0447b162f6d88c5b3af8c7b6fd92e0e7a67fe007
Ernest Irabor <irabeny89@gmail.com>
2021-05-13 20:24:12 +0100

    restyle

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,11 +5,11 @@ const Layout: FC = ({ children }) => (
 		<header className="bg-primary text-white p-1 font-italic">
 			<h1>Doge Reward</h1>
 		</header>
-		<main className="p-2" style={{
+		<main className="p-2 mr-lg-5" style={{
 			minHeight: "100vh",
 			display: "flex",
 			alignItems: "center",
-			justifyContent: "center"
+			justifyContent: "flex-end"
 		}}>{children}</main>
 		<footer className="bg-secondary text-white pl-1">&copy; 2021</footer>
 	</div>
